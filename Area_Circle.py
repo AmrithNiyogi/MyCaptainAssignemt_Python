5793297b60269a414b2024996590ec1e549665b7
AmrithNiyogi <70276074+AmrithNiyogi@users.noreply.github.com>
2022-03-14 20:58:13 +0530

    Add files via upload

---
+++ Area_Circle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  7 20:19:29 2022
+
+@author: amrit
+"""
+
+radius = float(input("Enter radius of the circle : "))
+
+area = 3.14 * (radius**2)
+
+print("Area of the circle is: ", area)
